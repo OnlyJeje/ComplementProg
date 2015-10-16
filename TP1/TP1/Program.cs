a1c88c78ce9cd64861238b2268fdfcbdb3fba6b1
OnlyJeje <jeremy.bailly@epitech.eu>
2015-10-16 16:57:11 +0800

    Debut

---
+++ TP1/TP1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1 {
+    unsafe class Program {
+        static void Main(string[] args) {
+            TableauEntiersSurLeTas tango = new TableauEntiersSurLeTas();
+
+            tango.AjoutEntier(0);
+            tango.AjoutEntier(1);
+            tango.AjoutEntier(2);
+            tango.AjoutEntier(3);
+            tango.AjoutEntier(4);
+            tango.AjoutEntier(5);
+            tango.AjoutEntier(6);
+            tango.AjoutEntier(7);
+
+            tango.AjoutEntier(8);
+            tango.AjoutEntier(9);
+            tango.AjoutEntier(10);
+            tango.AjoutEntier(11);
+            tango.AjoutEntier(12);
+            tango.AjoutEntier(13);
+            tango.AjoutEntier(14);
+            tango.AjoutEntier(15);
+            tango.AfficherTableau();
+            tango.Dispose();
+        }
+    }
+}
---
+++ TP1/TP1/TableauEntiersSurLeTas.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1 {
+    public unsafe class TableauEntiersSurLeTas : IDisposable {
+
+        private int* tab;
+        private int tabSize = 0;
+
+        public TableauEntiersSurLeTas() {
+            tab = (int *)Memory.Alloc(10 * sizeof(int));
+        }
+        ~TableauEntiersSurLeTas() {
+
+        }
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing) {
+            if (disposing) {
+                Memory.Free(tab);
+            }
+        }
+
+        public void AfficherTableau() {
+            for (int i = 0; i <= tabSize - 1; i++) {
+                Console.WriteLine("Element "+ i + " = " + tab[i]);
+            }
+            Console.ReadKey();
+        }
+
+        public void AjoutEntier(int nb) {
+            if ((tabSize > 9))
+            {
+                int* tab2 = (int*)Memory.Alloc(Memory.SizeOf(tab) + sizeof(int));
+                Memory.Copy(tab, tab2, Memory.SizeOf(tab));
+                tab = tab2;
+            } 
+            tab[tabSize] = nb;
+            tabSize++;           
+        }
+
+        public void EnleverEntier(int x) {
+
+        }
+    }
+}
