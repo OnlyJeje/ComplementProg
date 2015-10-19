b47c94a7be48a2caee2111a2ebe29b154f83373d
dechev_a <dechev_a@git.epitech.eu>
2015-10-18 22:18:43 -0400

    Generique
    Création du tableau générique, ajout/suppression d'éléments, affichage
    ainsi que reflexion primaire.
    
    A faire:
    Tri
    implémentation du delegate

--- TP1/TP1/Program.cs
+++ TP1/TP1/Program.cs
@@ -19,7 +19,25 @@ namespace TP1 {
             tango.Trier();
             tango.AfficherTableau();
             tango.Dispose();
-            
+
+            TableauGeneric<string> banjo = new TableauGeneric<string>();
+            banjo.addElement("plop");
+            banjo.addElement("plop");
+            banjo.addElement("plop");
+            banjo.addElement("plop");
+            banjo.addElement("plop");
+            banjo.addElement("plop");
+            banjo.addElement("boum");
+            banjo.addElement("plop");
+            banjo.addElement("plop");
+            banjo.addElement("plop");
+            banjo.addElement("plop");
+            banjo.addElement("plop");
+            Console.WriteLine(banjo.getElem(6));
+            banjo.deleteElement(6);
+            Console.WriteLine(banjo.getElem(6));
+            Console.WriteLine(banjo.reflexion());
+            Console.WriteLine("Fin"); 
         }
     }
 }
--- TP1/TP1/TableauGeneric.cs
+++ TP1/TP1/TableauGeneric.cs
@@ -4,7 +4,80 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TP1 {
-    class TableauGeneric {
+namespace TP1
+{
+
+    public unsafe class TableauGeneric<T>
+    {
+        private int size = 0;
+//        delegate bool handler<T> (T elem1, T elem2);
+        //Delegate handler = getsize;
+
+        public T[] tab;
+        public TableauGeneric ()
+        {
+            tab = new T[10];
+        }
+
+        public void addElement(T elem)
+        {
+            if (size >= 10)
+            {
+                T[] tab2 = new T[size + 1];
+                tab.CopyTo(tab2, 0);
+                tab = tab2;
+            }
+            tab[size] = elem;
+            size++;
+        }
+
+        public void deleteElement(int index)
+        {
+            T[] tab2 = new T[size - 1];
+            int i = 0;
+            while (i < index)
+            {
+                tab2[i] = tab[i];
+                i++;
+            }
+            i++;
+            while (i < tab.Length)
+            {
+                tab2[i - 1] = tab[i];
+                i++;
+            }
+            tab = tab2;
+        }
+        public T getElem(int index)
+        {
+            return (tab[index]);
+        }
+      /*  public void trions()
+        {
+            if (this.handler(tab[0], tab[1]) == true)
+            {
+
+            }
+        }*/
+
+        public bool getsize(T elem, T elem2)
+        {
+
+            if (elem.ToString().Length > elem2.ToString().Length)
+                return (true);
+            else
+                return (false);
+        }
+
+        public string reflexion()
+        {
+            string knowledge;
+            knowledge = "Type : ";
+            knowledge += (tab[0].GetType()).ToString();
+            knowledge += "\nAssembly : ";
+            System.Reflection.Assembly info = (tab[0].GetType()).Assembly;
+            knowledge += info.ToString();
+            return (knowledge);
+        }
     }
 }
