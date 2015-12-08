2c6c1a37e8ee416383627cb0ef9a925029d97b28
OnlyJeje <jeremy.bailly@epitech.eu>
2015-12-07 19:03:03 -0500

    Modificateur d'accès
    Récupération des modificateur d'accès + affichage.

--- TP2/TP2/Form1.cs
+++ TP2/TP2/Form1.cs
@@ -13,19 +13,88 @@ using System.IO;
 namespace TP2 {
 
     public partial class Form1 : Form {
-        enum test {
-            A,
-            B,
-            C
-        };
+        List<string> Display = new List<string>();
         public Form1() {
             InitializeComponent();
+
         }
 
+        public void ManageAssembly(Type t) {
+
+            /* Get Assembly Name*/
+            Display.Add("Nom: \n    " + t.Name);
+
+            Display.Add("\nAttributs personnalisés:");
+            /* Get assembly custom attribute and add to list */
+            foreach (var elemen in Attribute.GetCustomAttributes(t).ToArray()) {
+                Display.Add("   " + elemen);
+            }
+
+            /* Get assembly type name */
+            Display.Add("\nType : \n  " + t.BaseType.Name);
+
+            /* Get assembly's modifier access */
+            if (t.IsPublic)
+                Display.Add("\nModificateur d'accès : \n    Public ");
+            else if (t.IsNotPublic)
+                Display.Add("\nModificateur d'accès : \n    Private ");
+            else if (t.IsAbstract)
+                Display.Add("\nModificateur d'accès : \n    Abstract ");
+
+            Display.Add("\nFields :");
+        }
+
+
+        public void ManageField(Type t) {
+            FieldInfo[] field = t.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+            foreach (var f in field) {
+                Display.Add("   Nom: " + f.Name);
+                Display.Add("   Type : " + f.FieldType.Name);
+
+                /* Get field 's access modifier */
+                if (f.IsPublic)
+                    Display.Add("\nModificateur d'accès : \n    Public ");
+                else if (f.IsPrivate)
+                    Display.Add("\nModificateur d'accès : \n    Private ");
+                else if (f.IsFamily)
+                    Display.Add("\nModificateur d'accès : \n    Protected ");
+
+                foreach (var attr in Attribute.GetCustomAttributes(f).ToArray()) {
+                    Display.Add("   Attribut:" + attr.ToString());
+                }
+            }
+        }
+
+        public void ManageMethod(Type t) {
+            Display.Add("Méthodes: ");
+            /* Get assembly method and add to list */
+            MethodInfo[] method = t.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+            foreach (var m in method) {
+                Display.Add("   Nom : " + m.Name);
+                Display.Add("   Type de retour: " + m.ReturnType.Name);
+
+                /* Get method 's access modifier */
+                if (m.IsPublic)
+                    Display.Add("\nModificateur d'accès : \n    Public ");
+                else if (m.IsPrivate)
+                    Display.Add("\nModificateur d'accès : \n    Private ");
+                else if (m.IsFamily)
+                    Display.Add("\nModificateur d'accès : \n    Protected ");
+                foreach (var attr in Attribute.GetCustomAttributes(m).ToArray()) {
+                    Display.Add("       Attribut:" + attr.ToString());
+                }
+
+                Display.Add("   Paramètres: ");
+                /* Get method parameter and add to list */
+                foreach (var param in m.GetParameters()) {
+                    Display.Add("       Nom: " + param.Name);
+                    Display.Add("       Type: " + param.ParameterType.Name + "\n");
+                }
+            }
+        }
         private void button1_Click(object sender, EventArgs e) {
 
 
-            List<string> Display = new List<string>();
             OpenFileDialog openFileDialog = new OpenFileDialog();
 
             openFileDialog.Filter = "Exe File (.exe) |*.exe";
@@ -40,49 +109,11 @@ namespace TP2 {
 
                     foreach(Type t in file.GetTypes()) {
 
-                        /* Get Assembly Name*/
-                        Display.Add("Nom: \n    " + t.Name);
-
-                        Display.Add("\nAttributs personnalisés:");
-                        /* Get assembly custom attribute and add to list */
-                        foreach (var elemen in Attribute.GetCustomAttributes(t).ToArray()) {
-                            Display.Add("   " + elemen);
-                        }
-
-                        /* Get assembly type name */
-                        Display.Add("\nType : \n  " + t.BaseType.Name);
-
-                        Display.Add("\nFields :");
+                        ManageAssembly(t);
                         /* Get assembly fields and add to list */
-                        FieldInfo[] field = t.GetFields(BindingFlags.Public | BindingFlags.NonPublic |
-                   BindingFlags.Instance | BindingFlags.Static);
-                        foreach (var f in field) {
-                            Display.Add("   Nom: " + f.Name);
-                            Display.Add("   Type : " + f.FieldType.Name);
-                            
-                            foreach (var attr in Attribute.GetCustomAttributes(f).ToArray()) {
-                                Display.Add("   Attribut:" + attr.ToString());
-                            }                           
-                        }
+                        ManageField(t);
+                        ManageMethod(t);
 
-                        Display.Add("Méthodes: ");
-                        /* Get assembly method and add to list */
-                        MethodInfo[] method = t.GetMethods(
-                   BindingFlags.Public | BindingFlags.NonPublic |
-                   BindingFlags.Instance | BindingFlags.Static);
-                        foreach (var m in method){
-                            Display.Add("   Nom : " + m.Name);
-                            Display.Add("   Type de retour: " + m.ReturnType.Name);
-                            foreach (var attr in Attribute.GetCustomAttributes(m).ToArray()) {
-                                Display.Add("       Attribut:" + attr.ToString());
-                            }
-                            Display.Add("   Paramètres: ");
-                            /* Get method parameter and add to list */
-                            foreach (var param in m.GetParameters()) {
-                                Display.Add("       Nom: " + param.Name);
-                                Display.Add("       Type: " + param.ParameterType.Name + "\n");
-                            }
-                        }
                         Display.Add("\n-------------------------------------------------\n");
                 }
 
